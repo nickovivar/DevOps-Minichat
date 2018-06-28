327159064f459bd02cbfb7ddf6112c5b4bbab640
Javier Casas <javcasas@gmail.com>
2018-06-27 20:28:10 -0500

    Change logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './sb.png';
 import './App.css';
 import Login from './Login';
 import Chat from './Chat';
